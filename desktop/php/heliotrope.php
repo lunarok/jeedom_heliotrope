d528e316bbccb2762cccd8578f662ecee451a4b5
Cédric Dupont <lunarok@gmail.com>
2019-06-28 12:58:49 +0200

    Update heliotrope.php

--- desktop/php/heliotrope.php
+++ desktop/php/heliotrope.php
@@ -78,7 +78,7 @@ $eqLogics = eqLogic::byType('heliotrope');
                                 <select class="form-control eqLogicAttr" data-l1key="object_id">
                                     <option value="">{{Aucun}}</option>
                                     <?php
-                                    foreach (object::all() as $object) {
+                                    foreach (jeeObject::all() as $object) {
                                         echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
                                     }
                                     ?>
